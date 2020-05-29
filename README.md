d6bb092b0628e76707ad4769a3ebdbbf032d441f
Nina Hahne <ninamariehahne@gmail.com>
2020-05-29 02:59:16 +0200

    main setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AllThatStuff_v3
 AllThatStuff with a game lobby
+
+setup for game lobby inspired by https://github.com/InsomniacSabbir/anagrammatix
---
+++ index.js
@@ -0,0 +1,27 @@
+// Import the Express module
+const express = require("express");
+
+// Create a new instance of Express
+const app = express();
+
+// Import the AllthatStuff game file.
+const game = require("./game");
+
+// Serve static html, js, css, and image files from the 'public' directory:
+app.use(express.static("./public"));
+
+// // Create a Node.js based http server on port 8080
+const server = require("http")
+  .createServer(app)
+  .listen(process.env.PORT || 8080, () =>
+    console.log("port 8080 listening! - AllThatStuff")
+  );
+
+// Create a Socket.IO server and attach it to the http server
+var io = require("socket.io").listen(server);
+
+// Listen for Socket.IO Connections. Once connected, start the game logic.
+io.sockets.on("connection", function(socket) {
+  //console.log('client connected');
+  game.initGame(io, socket);
+});
